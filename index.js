b1a1e016b7edfa07709fd56ce6103112f0b37003
Tsurematsu <daniel120802@gmail.com>
2024-08-13 21:07:19 -0500

    changes1

--- index.js
+++ index.js
@@ -15,18 +15,51 @@ async function main() {
         await agregarMediaAlPeerConnection(peerClient, 'audio');
         await agregarMediaAlPeerConnection(peerClient, 'video');
       },
+      onClosedClient:liberarStreams,
     });
     await peerConector.start();
     getButton.addEventListener('click', async () => {console.log('connections ->', connections);});
 }
 main()
+const  streamUser = {};
+
+async function liberarStreams(peerClient, idClient) {
+  try {
+    const videoClient = document.getElementById(idClient)
+    videoClient.srcObject = null;
+    videoClient.remove();
+    // const senders = peerClient.getSenders();
+    // senders.forEach(sender => {
+    //   if (sender.track) {
+    //     sender.track.stop();
+    //   }
+    // });
+  } catch (error) {
+    console.error('Error al liberar los streams:', error);
+  }
+}
 
 async function agregarMediaAlPeerConnection(peerClient, tipoMedia) {
     try {
       const constraints = tipoMedia === 'audio'  ? {audio: true, video: false} : {audio: false, video: true};
-      const stream = await navigator.mediaDevices.getUserMedia(constraints);
-      stream.getTracks().forEach(track => {
-        peerClient.addTrack(track, stream);
+      if (streamUser[tipoMedia] == undefined) {
+        streamUser[tipoMedia] = await navigator.mediaDevices.getUserMedia(constraints);
+        if (tipoMedia === 'video') {
+          if (document.getElementById('MainId') !== null) {document.getElementById('MainId').remove();  }
+          const video = document.createElement('video');
+          video.autoplay = true;
+          video.muted = true;
+          video.style.width = '200px';
+          video.style.height = '200px';
+          video.style.margin = '10px';
+          video.id = 'MainId';
+          video.style.backgroundColor = 'white';
+          video.srcObject = streamUser[tipoMedia];
+          document.body.appendChild(video);
+        }
+      }
+      streamUser[tipoMedia].getTracks().forEach(track => {
+        peerClient.addTrack(track, streamUser[tipoMedia]);
       });
     } catch (error) {}
   }
